1ef6c005dfd0ce7ef47726bd46abe7d2fd1efb08
sumclog <sumc-279404@proton.me>
2026-06-03 14:51:15 +0300

    Add project files.

---
+++ MyImageViewer/Utils/ImageHelper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyImageViewer.Utils
+{
+    internal class ImageHelper
+    {
+    }
+}
---
+++ MyImageViewer/models/ImageFile.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyImageViewer.models
+{
+    internal class ImageFile
+    {
+    }
+}
